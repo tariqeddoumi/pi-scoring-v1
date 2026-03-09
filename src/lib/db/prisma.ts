30da50fe28e2dd18d20912e177aebc112126306e
tariqeddoumi <139554666+tariqeddoumi@users.noreply.github.com>
2026-03-09 13:39:33 +0000

    Update prisma.ts

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -7,7 +7,9 @@ const globalForPrisma = global as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query", "error", "warn"],
+    log: ["error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
