b12bdd9ee2a54a4ebc4edbaad343b59ddfe700e9
tariqeddoumi <139554666+tariqeddoumi@users.noreply.github.com>
2026-03-09 12:37:04 +0000

    Update prisma.ts

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var __prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  global.__prisma ??
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error", "warn"],
+    log: ["query", "error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") global.__prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
