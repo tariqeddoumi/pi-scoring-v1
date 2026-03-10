a3e16de729ad95538106c22e0a5c1644579a9a51
tariqeddoumi <139554666+tariqeddoumi@users.noreply.github.com>
2026-03-10 08:51:24 +0000

    Set dynamic export to force-dynamic

--- src/app/projects/[id]/evaluate/page.tsx
+++ src/app/projects/[id]/evaluate/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { notFound, redirect } from "next/navigation";
 import { prisma } from "@/lib/db/prisma";
 import { loadModelFromDb } from "@/lib/domain/loadModelFromDb";
